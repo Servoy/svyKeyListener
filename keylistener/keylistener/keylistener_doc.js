cf7284922cb45ca415be9e0e6f442155bcd4085b
mvid <mvid@servoy.com>
2025-03-31 17:59:52 +0300

    SVY-19926 SpecMarkdownGenerator - incomplete/missing documentation for handlers

--- keylistener/keylistener/keylistener_doc.js
+++ keylistener/keylistener/keylistener_doc.js
@@ -1,4 +1,14 @@
 /**
+ * A Servoy Extra Component that handles keyboard events and provides key listener functionality.
+ */
+
+/**
+ * Global variable that holds key listener callback configurations.
+ */
+var callbacks;
+
+
+/**
  * Registers the listener on the components which have the "keylistener" attribute with the given callbackKey value, to call the given callback when a key is pressed.
  *
  * @param {String} callbackKey The key for which the listener will be added. This key has to match value of "keylistener" attribute in component.
@@ -28,19 +38,33 @@ function removeKeyListener(callbackKey) {
 
 var svy_types = {
 
+    /**
+     * Represents a key listener callback configuration.
+     */
     callback: {
-
-        callbackKey : null,
-
-        callback : null,
-
-        isRunning : null,
-
-        delay : null,
-
-        regexPattern : null,
-
-        regexReplacement : null,
-
+        /**
+         * The key identifier for the callback. This should match the "keylistener" attribute value in the component.
+         */
+        callbackKey: null,
+        /**
+         * The callback function that will be executed when the key event occurs.
+         */
+        callback: null,
+        /**
+         * Indicates whether the callback is currently running.
+         */
+        isRunning: null,
+        /**
+         * The delay in milliseconds before the callback is executed after the key event.
+         */
+        delay: null,
+        /**
+         * A JavaScript regular expression string used to modify the current value of the component.
+         */
+        regexPattern: null,
+        /**
+         * The string used to replace the parts of the component's value that match the regexPattern.
+         */
+        regexReplacement: null
     }
 }
